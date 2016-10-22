03659d8e572c0521ba0b9dba53a3f5c955fa007d
noufalsalim <noufalsalim@icloud.com>
2016-10-22 21:36:42 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#This is my first project on GitHub
+#Everyone who uses computer knows the first program is 'Hello World'
+#This file and everything else i post on this page is my own orginal work
+#No copyright is violated by posting these projects
+# python 2.7.11
+# Anaconda
+# Linux user with Ubuntu 16.04LTS
+
+print "Hello World" # this will print hello world with a new line
